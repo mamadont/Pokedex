7c76811b2bf55e7356c1f1339c9da8091fd215b0
Mamadou Coulibaly <mamadoucoulibaly@Mamadous-MacBook-Pro.local>
2020-05-26 17:30:39 -0400

    Finished

--- script.ts
+++ script.ts
@@ -4,7 +4,7 @@ const container: HTMLElement | any = document.getElementById("app")
 const pokemons: number = 100; 
 
 // Defines the structure of the pokemon card 
-interface Pokemon {
+interface IPokemon {
     id: number;
     name: string;
     image: string;
@@ -38,6 +38,21 @@ const getPokemon = async (id: number): Promise<void> => {
         type: pokemonType,
       }
     
-    //showPokemon(transformedPokemon)
-
+    showPokemon(transformedPokemon)
 }
+
+// Dynamically updates the markup of the html page 
+const showPokemon = (pokemon: IPokemon): void => {
+    let output: string = `
+    <div class="card">
+        <span class="card--id">#${pokemon.id}</span>
+        <img class="card--image" src=${pokemon.image} alt=${pokemon.name} />
+        <h1 class="card--name">${pokemon.name}</h1>
+        <span class="card--details">${pokemon.type}</span>
+    </div>
+    `
+    // Appends
+    container.innerHTML += output 
+}
+
+fetchData()
